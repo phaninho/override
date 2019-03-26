27cc7bf91abadb24f1ac9bd71e0649b004812b7d
phaninho <phaninho@live.fr>
2019-03-26 19:17:09 +0100

    update endian converter

--- level02/endian_convert.c
+++ level02/endian_convert.c
@@ -6,8 +6,9 @@ int main(int ac, char **av)
         return 1;
     for (int i = 1; i < ac; i++)
     {
-        for (int j = 15; j > 0; j = j - 2)
-            printf("%c%c", av[i][j- 1],  av[i][j]);
+        for (int j = 17; j > 0; j = j - 2)
+			if (av[i][j - 1] != '0' && av[i][j] != 'x')
+				printf("%c%c", av[i][j- 1],  av[i][j]);
         printf("\n");
     }
     return 0;
